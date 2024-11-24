c264862bd88a7e668abf5843527e283357df85e7
Rohit Agarwal <rohit.net@gmail.com>
2024-11-24 08:43:11 -0800

    Handle concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![allow(unused_imports)]
 use std::{
     io::{Read, Write},
-    net::TcpListener,
+    net::{TcpListener,TcpStream}
 };
 
 fn main() {
@@ -10,25 +10,31 @@ fn main() {
     // stream represents the incoming connection
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => { // 'mut' allows us to modify the stream object
+            Ok(stream) => { // 'mut' allows us to modify the stream object
                 println!("accepted new connection");
 
-                let mut buf = [0; 512];
-                loop {
-                    let read_count = stream.read(&mut buf).unwrap();
-                    if read_count == 0 {
-                        break;
-                    }
-
-                    // "b" make it a byte string
-                    // "unwrap" assert that no errors occur during the write operation.
-                    // If an error does occur, it will cause the program to panic and exit.
-                    stream.write_all(b"+PONG\r\n").unwrap();
-                }
+                // spawns a new thread for each incoming connection
+                // 'move' lets the closure take ownership of 'stream'
+                let _ = std::thread::spawn(move || handle_client(stream));
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
+    loop {
+        let read_count = stream.read(&mut buf).unwrap();
+        if read_count == 0 {
+            break;
+        }
+
+        // "b" make it a byte string
+        // "unwrap" assert that no errors occur during the write operation.
+        // If an error does occur, it will cause the program to panic and exit.
+        stream.write_all(b"+PONG\r\n").unwrap();
     }
 }
