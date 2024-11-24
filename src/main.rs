ad91b1a4008b5e53ee26a29e33ce523e52ade2f9
Rohit Agarwal <rohit.net@gmail.com>
2024-11-24 07:49:26 -0800

    respond to ping

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
+    // stream represents the incoming connection
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => { // 'mut' allows us to modify the stream object
                 println!("accepted new connection");
+
+                let mut buf = [0; 512];
+                stream.read(&mut buf).unwrap();
+
+                // "b" make it a byte string
+                // "unwrap" assert that no errors occur during the write operation.
+                // If an error does occur, it will cause the program to panic and exit.
+                stream.write_all(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
