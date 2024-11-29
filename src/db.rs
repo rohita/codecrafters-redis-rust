098a3c87afcab8fee3ef6855dc6750fca84cb36f
Rohit Agarwal <rohit.net@gmail.com>
2024-11-29 14:56:13 -0800

    Read value with expiry #sm4

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::time::Instant;
+use std::time::{SystemTime, UNIX_EPOCH};
 use std::fs;
 use std::fs::File;
 use std::io::Read;
@@ -71,7 +72,33 @@ fn read_file(config: HashMap<String, String>) -> HashMap<String, Item> {
         println!("Hashtable Size: {}, Expire Size: {}", hashtable_size, expire_hashtable_size);
 
         for _ in 0..hashtable_size {
-            let _value_type = iter.next();
+            let mut value_type = iter.next().unwrap();
+            let expiry: u128;
+
+            match value_type {
+                0xFC => {
+                    let mut temp_expiry:[u8; 8] = [0; 8];
+                    for i in 0..8 {
+                        temp_expiry[i] = iter.next().unwrap();
+                    }
+                    expiry = u64::from_le_bytes(temp_expiry) as u128;
+                    value_type = iter.next().unwrap();
+                }
+                0xFD => {
+                    let mut temp_expiry:[u8; 4] = [0; 4];
+                    for i in 0..4 {
+                        temp_expiry[i] = iter.next().unwrap();
+                    }
+                    expiry = (u32::from_le_bytes(temp_expiry) * 1000) as u128;
+                    value_type = iter.next().unwrap();
+                }
+                _ => {
+                    // no expiry
+                    expiry = 0;
+                }
+            };
+
+            println!("KV type: {}, expiry: {:?}", value_type, expiry);
 
             let key_len = iter.next().unwrap();
             let mut key_chars = Vec::new();
@@ -88,10 +115,16 @@ fn read_file(config: HashMap<String, String>) -> HashMap<String, Item> {
             let value = value_chars.into_iter().collect();
 
             println!("Loaded from file = key: {:?}, value: {:?}", key, value);
+
+            let in_ms = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
+            if expiry > 0 && expiry < in_ms {
+                continue;
+            }
+
             storage.insert(key, Item {
                 value,
                 created: Instant::now(),
-                expires: 0,
+                expires: expiry - in_ms,
             });
         }
     }
